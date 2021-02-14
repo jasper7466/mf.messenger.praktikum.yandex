b9c7d8b7dc5334fd412f1b487272fd49869fc405
Tim <jasper7466@yandex.ru>
2021-02-14 08:13:16 +0500

    Express. Изменена целевая директория (./src/pages -> ./src)

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
-app.use(express.static('./src/pages'));
+app.use(express.static('./src'));
 
 app.listen(PORT, function () {
   console.log(`App listening on port ${PORT}!`);
