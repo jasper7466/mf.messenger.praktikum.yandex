edd0cdc340e106729244ef19cd866c92c7268187
Tim <jasper7466@yandex.ru>
2021-02-28 11:30:47 +0500

    Логирование форм. Bug-fix + перемещение в "./utilities"

--- src/scripts/utilities/form-logger.ts
+++ src/scripts/utilities/form-logger.ts
@@ -4,8 +4,8 @@ if (form) {
     const fields = form.querySelectorAll('input')
 
     form.addEventListener('submit', event => {
-        let data = {};
-        fields.forEach(field => Object.defineProperty(data, field.name, field.value));
+        let data: any = {};
+        fields.forEach(field => data[field.name] =  field.value);
         console.log(data);
         event.preventDefault();
     });
