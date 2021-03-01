e39096225540ddfbf6ff00f34630ad046f0a523e
Tim <jasper7466@yandex.ru>
2021-03-01 18:50:27 +0500

    Ревью. Express. Bug-fix. Изменена ссылка на директорию статики

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
-app.use(express.static('./src'));
+app.use(express.static('./dist'));
 
 app.listen(PORT, function () {
   console.log(`App listening on port ${PORT}!`);
