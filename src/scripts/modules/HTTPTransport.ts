b6a7b6d85a1e13911ca5d932d146d5374cf2497e
Tim <jasper7466@yandex.ru>
2021-03-18 18:19:32 +0500

    Реализован класс "HTTPTransport"

---
+++ src/scripts/modules/HTTPTransport.ts
@@ -0,0 +1,80 @@
+type StringObj = {
+    [key: string]: string
+};
+
+type Options = {
+    data?: StringObj,
+    timeout?: number
+}
+
+type RequestOptions = {
+    headers?: object,
+    data?: StringObj,
+    method: string,
+}
+
+export class HTTPTransport {
+
+    private static METHODS = {
+        GET: 'GET',
+        POST: 'POST',
+        PUT: 'PUT',
+        DELETE: 'DELETE'
+    };
+
+    constructor(readonly _baseURL: string) {}
+
+    get = (url: string, options: Options = {}) => {
+        return this.request(url, {...options, method: HTTPTransport.METHODS.GET}, options.timeout);
+    };
+
+    post = (url: string, options: Options = {}) => {
+        return this.request(url, {...options, method: HTTPTransport.METHODS.POST}, options.timeout);
+    };
+
+    put = (url: string, options: Options = {}) => {
+        return this.request(url, {...options, method: HTTPTransport.METHODS.PUT}, options.timeout);
+    };
+
+    delete = (url: string, options: Options = {}) => {
+        return this.request(url, {...options, method: HTTPTransport.METHODS.DELETE}, options.timeout);
+    };
+
+    request = (url: string, options: RequestOptions, timeout: number = 5000): Promise<any> => {
+        const {headers = {}, method, data} = options;
+        url = `${this._baseURL}/${url}`;
+
+        if (data !== undefined)
+            url += HTTPTransport.queryStringify(data);
+
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open(method, url);
+            xhr.onload = () => resolve(xhr);
+            xhr.onabort = reject;
+            xhr.onerror = reject;
+            xhr.ontimeout = reject;
+            xhr.timeout = timeout;
+            xhr.withCredentials = true;
+            xhr.responseType = 'json';
+
+            if (headers)
+                Object.entries(headers).forEach(header => xhr.setRequestHeader(header[0], header[1]));
+
+            if (method === HTTPTransport.METHODS.GET || !data)
+                xhr.send();
+            else
+                xhr.send(data as unknown as FormData);
+        });
+    };
+
+    public static queryStringify(data: StringObj) {
+        if (typeof data === 'string')
+            return data;
+        const params = [];
+        for (let key in data) {
+            params.push(`${key}=${data[key]}`)
+        }
+        return `?${params.join('&')}`;
+    }
+}
