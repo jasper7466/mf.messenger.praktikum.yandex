2facafcb632edf28d221ad12273e17b9702b31cb
Tim <jasper7466@yandex.ru>
2021-03-23 20:09:40 +0500

    Files. Удалены файлы страниц ошибок 404/500

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import Router from './modules/Router';
 import { LoginPage } from './pages/login/index';
 import { ChatMainPage } from './pages/chat-main/index';
 import { ChatSelectPage } from './pages/chat-select/index';
-import { Error404Page } from './pages/error-404/index';
-import { Error500Page } from './pages/error-500/index';
 import { ErrorPage } from './pages/error/index';
 import { ProfilePage } from './pages/profile/index';
 import { ProfileDataPage } from './pages/profile-data/index';
@@ -12,8 +10,6 @@ import { SignupPage } from './pages/signup/index';
 import { data as loginContext } from './pages/login/index.tmpl';
 import { data as chatMainContext } from './pages/chat-main/index.tmpl';
 import { data as chatSelectContext } from './pages/chat-select/index.tmpl';
-import { data as error404Context } from './pages/error-404/index.tmpl';
-import { data as error500Context } from './pages/error-500/index.tmpl';
 import { data as profileContext } from './pages/profile/index.tmpl';
 import { data as profileDataContext } from './pages/profile-data/index.tmpl';
 import { data as profilePasswordContext } from './pages/profile-password/index.tmpl';
@@ -38,8 +34,6 @@ router
     .use(Routes.login, LoginPage, loginContext)
     .use(Routes.chatMain, ChatMainPage, chatMainContext)
     .use(Routes.chatSelect, ChatSelectPage, chatSelectContext)
-    .use(Routes.error404, Error404Page, error404Context)
-    .use(Routes.error500, Error500Page, error500Context)
     .use(Routes.error, ErrorPage, {})
     .use(Routes.profile, ProfilePage, profileContext)
     .use(Routes.profileData, ProfileDataPage, profileDataContext)
