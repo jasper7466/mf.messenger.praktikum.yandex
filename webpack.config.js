da2f54e1e99734851ce807a087af58a7f0d53ceb
Tim <jasper7466@yandex.ru>
2021-04-10 18:32:30 +0500

    Webpack. Добавлена поддержка less, sass, оптимизация css

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,53 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin;
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
+const isDevMode = process.env.NODE_ENV === 'development';
+const isProdMode = !isDevMode;
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+
+    if (isProdMode) {
+        config.minimizer = [
+            new OptimizeCssAssetWebpackPlugin(),
+            new TerserWebpackPlugin()
+        ];
+    }
+
+    return config;
+}
+
+const filename = ext => isDevMode ? `[name].${ext}` : `[name].bundle.[contenthash].${ext}`;
+
+const cssLoaders = extra => {
+    const loaders = [
+        {
+            loader: MiniCssExtractPlugin.loader,
+            options: {},
+        },
+        'css-loader'
+    ];
+
+    if (extra)
+        loaders.push(extra);
+
+    return loaders;
+}
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
     entry: { app: './index.ts' },
     output: {
-        filename: '[name].bundle.[contenthash].js',
+        filename: filename('js'),
         path: path.resolve(__dirname, 'dist'),
     },
     resolve: {
@@ -22,24 +62,29 @@ module.exports = {
             '@': path.resolve(__dirname, 'src'),
         }
     },
-    optimization: {
-        splitChunks: {
-            chunks: 'all'
-        }
-    },
+    optimization: optimization(),
     plugins: [
-        new HtmlWebpackPlugin({ template: '../static/index.html' }),
+        new HtmlWebpackPlugin({
+            template: '../static/index.html',
+            minify: {
+                collapseWhitespace: isProdMode,
+            }
+        }),
         new CleanWebpackPlugin(),
         new CopyWebpackPlugin({
             patterns: [{
                 from: path.resolve(__dirname, 'static/images'),
                 to: path.resolve(__dirname, 'dist/images'),
             }]
+        }),
+        new MiniCssExtractPlugin({
+            filename: filename('css')
         })
     ],
     devServer: {
         port: 4000,
         open: true,
+        hot: isDevMode,
         host: 'localhost',
     },
     module: {
@@ -50,9 +95,16 @@ module.exports = {
                 exclude: ['/node_modules/'],
             },
             {
-                test: /\.css$/i,
-                include: path.resolve(__dirname, './static/styles'),
-                use: ['style-loader', 'css-loader', 'postcss-loader'],
+                test: /\.css$/,
+                use: cssLoaders('postcss-loader')
+            },
+            {
+                test: /\.less$/,
+                use: cssLoaders('less-loader')
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: cssLoaders('sass-loader')
             },
             {
                 test: /\.(eot|ttf|woff|woff2)$/,
