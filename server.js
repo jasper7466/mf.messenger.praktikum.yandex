d08f25a43f956bbc286ab19be69179c9bcda3de6
Tim <jasper7466@yandex.ru>
2021-04-13 21:35:22 +0500

    Heroku. Тестовый коммит (отладка)

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const PORT = 4000;
 
 app.use(express.static('./dist'));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 app.listen(PORT, function () {
   console.log(`App listening on port ${PORT}!`);
-}); 
+});
